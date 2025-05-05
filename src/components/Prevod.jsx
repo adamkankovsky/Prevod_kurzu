7e0ff2aa4100efcf9f612ea96f0123f1c879ad9c
Kaňkovský Adam <kankovsky@prumyslovkajihlava.cz>
2025-05-05 12:36:23 +0200

    Basic template

---
+++ src/components/Prevod.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+
+export const Prevod = () => {
+    const [cislo, setCislo] = useState(0);
+    const [kurzy, setKurzy] = useState([]);
+
+    const getKurzy = async () => {
+        try {
+            const response = await fetch("https://api.frankfurter.dev/v1/latest?base=EUR");
+            const data = await response.json();
+            setKurzy(data);
+        }
+    };
+    
+    useEffect(() => {
+        
+    }, []);
+
+    return(
+        <div>
+            <h1>
+                Převod EUR do jiné měny
+            </h1>
+            <div>
+                <input 
+                    type="number"
+                    value={cislo}
+                    onChange={(e) => setCislo(e.target.value)}
+                    style={{ padding: '5px' }}
+                />
+                <select></select>
+                <button>Převeď</button>
+            </div>
+        </div>
+    )
+}
